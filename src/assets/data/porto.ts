b39da70e99e5a4f8a9eac43ecdc245150849b6cd
Wahyu Nur Arizky <44398064+wahyunurarizky@users.noreply.github.com>
2023-11-20 17:52:31 +0700

    Update porto.ts

--- src/assets/data/porto.ts
+++ src/assets/data/porto.ts
@@ -10,14 +10,14 @@ export interface IPorto {
 export const porto: IPorto[] = [
   {
     name: 'Puskarier (2021)',
-    desc: 'application for job search and career development for UIN Jakarta alumni',
+    desc: 'Application for job search and career development for UIN Jakarta alumni',
     url: 'https://puskarier.vercel.app',
     image: '/porto/porto-puskarier.png',
     buildWith: ['React JS', 'Laravel', 'My SQL', 'SCSS']
   },
   {
     name: 'SIAPP (2021)',
-    desc: 'application for submission of research by UIN Jakarta researchers',
+    desc: 'Application for submission of research by UIN Jakarta researchers',
     url: 'https://siapp.uinjkt.ac.id',
     image: '/porto/porto-siapp.png',
     buildWith: ['Laravel', 'Tailwind', 'Bootstrap', 'MySQL']
@@ -40,14 +40,14 @@ export const porto: IPorto[] = [
   },
   {
     name: `Mom's Day Out Ceres Meses (2023)`,
-    desc: `create event website for Ceres Meses celebrate Mom's Day Out`,
+    desc: `Create event website for Ceres Meses celebrate Mom's Day Out`,
     url: 'https://momsdayout.ceresmeses.co.id',
     image: '/porto/porto-ceres.png',
     buildWith: ['Laravel', 'Inertia', 'ReactJS', 'Tailwind', 'Redis']
   },
   {
     name: `Berburu Vespa Kober Mie (2023)`,
-    desc: `sayembara mie kober app to get vespa`,
+    desc: `Sayembara mie kober app to get vespa`,
     url: 'https://berburuvespa.kobermie.com',
     image: '/porto/porto-kober.png',
     buildWith: ['Laravel', 'Inertia', 'ReactJS', 'Tailwind', 'Redis']
