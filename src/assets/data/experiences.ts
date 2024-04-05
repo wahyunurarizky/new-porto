98d048992689cf679e14707aedc572abd03f4810
wahyu nur arizky <wahyu.nur@sera.astra.co.id>
2024-04-05 09:23:56 +0700

    update experiences

--- src/assets/data/experiences.ts
+++ src/assets/data/experiences.ts
@@ -12,15 +12,7 @@ export const experiences: IExp[] = [
     company: 'UIN Jakarta research and publishing center',
     date: 'Jan - Aug 2021',
     description: `Create an application <a target="_blank" href="https://siapp.uinjkt.ac.id">siapp.uinjkt.ac.id</a> using laravel, this application is used for researchers and lecturers at UIN to submit research proposals and publications.`,
-    learns: [
-      'Laravel',
-      'Tailwind',
-      'Bootstrap',
-      'MySQL',
-      'Linux',
-      'Apache',
-      'Product planning'
-    ]
+    learns: ['Laravel', 'Tailwind', 'Bootstrap', 'MySQL', 'Linux', 'Apache', 'Product planning']
   },
   {
     title: 'Tech team intern',
@@ -34,32 +26,20 @@ export const experiences: IExp[] = [
     company: 'PT Falah Inovasi Teknologi',
     date: 'Des 2021 - Jun 2023',
     description: `1 year 6 months as fulltime backend developer, learn and use so many tools to reach the goal as backend developer.`,
-    learns: [
-      'Laravel',
-      'Express',
-      'PostgresSQL',
-      'Docker',
-      'Microservices',
-      'ERP Dev',
-      'Figma',
-      'Socket IO'
-    ]
+    learns: ['Laravel', 'Express', 'PostgresSQL', 'Docker', 'Microservices', 'ERP Dev', 'Figma', 'Socket IO']
   },
   {
     title: 'Software Engineer',
     company: 'Integrity Asia',
-    date: 'Jun 2023 - Present',
+    date: 'Jun 2023 - Feb 2024',
     description: `Integrity Asia provides efficient solutions for ensuring good governance and business certainty, improving fraud detection and swiftly investigating infringements. I have responsible to develop new app using NestJS and NextJS as fullstack developer.`,
-    learns: [
-      'NestJS',
-      'NextJS',
-      'ReactJS',
-      'MongoDB',
-      'Microservices',
-      'Rabbitmq',
-      'Azure devops',
-      'Websocket',
-      'Xendit payment'
-    ]
+    learns: ['NestJS', 'NextJS', 'ReactJS', 'MongoDB', 'Microservices', 'Rabbitmq', 'Azure devops', 'Websocket', 'Xendit payment']
+  },
+  {
+    title: 'Backend Develoeper',
+    company: 'PT Serasi Autoraya ( SERA )',
+    date: 'Feb 2024 - Present',
+    description: `Create new product TRAC as Backend developer`,
+    learns: ['Express JS', 'TypeScript', 'PostgresSQL', 'Microservices', 'AWS S3', 'AWS SQS', 'JIRA', 'Firebase', 'Xendit payment']
   }
 ]
